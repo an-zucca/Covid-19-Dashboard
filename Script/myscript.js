66a49f995f370c199ec3a506262223b3a40ec868
zuccaandrea <75362160+zuccaandrea@users.noreply.github.com>
2021-10-12 01:23:48 +0200

    Delete myscript.js

--- Script/myscript.js
+++
@@ -1,4 +0,0 @@
-Shiny.addCustomMessageHandler ('changeTxtColor',function (m) {
-                      var element = $('#'+m.id); // Find element to change color of
-                      element.css({ 'color': m.color}); // Change color of element
-              });
